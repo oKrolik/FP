2fb75fc4b65340ad8e4a100670e9247773f00ffb
oKrolik <gcoelho2002@gmail.com>
2022-10-03 15:42:27 +0100

    add means.py

---
+++ Playground/means.py
@@ -0,0 +1,6 @@
+"""
+Write a Python program that takes in three integers
+by user input and prints out their mean to the console.
+Please use round with two decimal places.
+"""
+print(round((int(input())+int(input())+int(input()))/3,2))
