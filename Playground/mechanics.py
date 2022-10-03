8f18bb09a153f4799785064c273a9a7d187d9938
oKrolik <gcoelho2002@gmail.com>
2022-10-03 15:34:08 +0100

    minor changes

--- Playground/mechanics.py
+++ Playground/mechanics.py
@@ -1,3 +1,23 @@
+"""
+A king needs your help to defend his castle!
+He will tell you the angle of his cannon (in degrees)
+and you will tell him how far (horizontally) the cannon
+ball will go (until it hits the ground).
+
+The cannon ball fires at the velocity of 18 m/s.
+Air resistance (drag) is negligible. Assume g=-10m/s².
+Please round the result.
+
+Some code is already provided for you:
+
+    import math
+
+    angle = int(input()) * math.pi / 180  # convert to radians
+    cos_angle = math.cos(angle)
+    sin_angle = math.sin(angle)
+
+"""
+
 import math
 angle = int(input())*math.pi/180  # convert to radians
 cos_angle = math.cos(angle)
