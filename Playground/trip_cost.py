9f5a91abb0c796fb1ed195d64febfa4a745cc0bc
oKrolik <gcoelho2002@gmail.com>
2022-10-04 14:47:55 +0100

    add trip_cost.py

---
+++ Playground/trip_cost.py
@@ -0,0 +1,7 @@
+"""
+You are budgeting for a car trip and need to know how much you will spend on fuel. 
+Write a Python program that receives three inputs: the distance to travel (integer), 
+the number of litres of fuel needed to travel 100 km (float), and the price per litre of fuel (float), 
+then prints the total cost of fuel for the trip. Use round with 2 decimal places.
+"""
+print(round(int(input())/100*float(input())*float(input()),2))
